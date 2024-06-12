a1c3d31328eea7eec2f3c4fda182a956407b23a8
Reihan Mud <93262457+ReihanMudd@users.noreply.github.com>
2024-06-11 21:53:43 -0400

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+      title: "AI Salesman Chatbot and Webscraper",
+      subtitle: "Python and Google Gemini",
+      description:
+        "A project that employs the usage of Google Gemini. It is a fully built ebay item webscraper that has built in functionality as a salesman chatbot through the useage and training of Google Gemini.",
+      image: "./AI.gif",
+      link: "https://github.com/ReihanMudd/AI-Project",
+    },
+    {
+      title: "2D Frog Game",
+      subtitle: "Godot and GDScript",
+      description:
+        "A simple 2D Frog platformer that allows the player to collect items to gain points. Once they reach they end, they can reach a trophy that leads them to a winning screen. Unfortunately, this is not an officially published game yet, so you will have to download and run the files on godot to play the game.",
+      image: "./2D_Game.gif",
+      link: "https://drive.google.com/file/d/1ZXE07-ZYWOQDoeAP_lDKzdc4y3Dux8g1/view?usp=sharing",
+    },
+    {
+      title: "Polymorphism Teaching Lab",
+      subtitle: "Java",
+      description:
+        "A simple lab that is centered around teaching the basics of polymorphism in Java. This is paired with a doc with instructions, as well as a blank Java file that students will have to make themselves. The program shown here is meant to be the solution to the lab.",
+      image: "./Barbie.gif",
+      link: "https://github.com/ReihanMudd/Polymorphism-Lab",
+    },
+    {
+      title: "Future Projects",
+      subtitle: "Work in Progres",
+      description:
+        "Future projects will be added as they are completed, so stay tuned for anything else I release!",
+      image: "./WIPS.svg",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "R",
+    "Java",
+    "C++",
+    "Python",
+    "HTML/CSS",
+  ];
